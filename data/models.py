78ec43d7fd31b9d3f54778644f02aef96f39396f
alireza <alirezashateri7@gmail.com>
2021-03-09 14:55:14 +0330

    Add CronJob model
    CronJob model added to save
    cron jobs added by user in
    database.

---
+++ data/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .db import Base
+
+
+class CronJob(Base):
+    __tablename__ = 'cron_jobs'
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(String, unique=True, index=True)
+
+    enabled = Column(Boolean, default=True)
