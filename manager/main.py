e500936ff43b5b5d5bfaff53f957537e59acb466
alireza <alirezashateri7@gmail.com>
2021-05-26 16:45:34 +0430

    Register tortoise orm on fastapi

--- manager/main.py
+++ manager/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
-
+from config import DATABASE_URL
 from routers import service_router
 
 app = FastAPI()
+
+register_tortoise(
+    app=app,
+    db_url=DATABASE_URL,
+    modules={'manager': ["data.db.models"]},
+    generate_schemas=False,
+    add_exception_handlers=True
+)
 
 app.include_router(
     service_router,
