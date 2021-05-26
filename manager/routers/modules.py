2792ab74a23c85ac5f0e6fcb529e63fbd575513c
alireza <alirezashateri7@gmail.com>
2021-05-26 16:40:27 +0430

    Add regsiter and get_regsitered routes
    The register route with post method to
    register new modules for cluster. You just
    need to give a unique name and manager
    will create a unique 64 character secret
    key for your module. Then you should
    put this secret key in module to use it
    for logging in the module to manager
    and joining it to cluster.

--- manager/routers/modules.py
+++ manager/routers/modules.py
@@ -1,19 +1,56 @@
 from typing import Optional, List
 
-from fastapi import APIRouter, Path, Body, Query
+from fastapi import APIRouter, Path, Body, Query, Response, status
+
+from data.db import (
+    Module as ModuleDB,
+    ModuleInstance as ModuleInstanceDB,
+    Token as TokenDB
+)
+from data.pydantic import (
+    ModuleInstance,
+    ModuleResponse
+)
 
 __all__ = ('router',)
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_model=List[ModuleResponse])
 async def get_registered():
-    pass
+    modules = await ModuleDB.all().prefetch_related('instances')
+
+    module_responses = await ModuleResponse.module_response_from_db_model_list(
+        modules
+    )
+
+    return module_responses
 
 
-@router.post("/")
-async def register():
+@router.post("/", response_model=ModuleResponse)
+async def register(
+        response: Response,
+        name: str = Body(..., title='Module name', max_length=128, embed=True)
+):
+    module, created = await ModuleDB.get_or_create(name=name)
+
+    await module.fetch_related('instances')
+
+    module_response = await ModuleResponse.module_response_from_db_model(
+        db_model=module
+    )
+
+    if created:
+        response.status_code = status.HTTP_201_CREATED
+    else:
+        response.status_code = status.HTTP_200_OK
+
+    return module_response
+
+
+@router.post("/login")
+async def login():
     pass
 
 
