9a0f3a7878967c2476eb8ef6d6eea4eacdedba30
alireza <alirezashateri7@gmail.com>
2021-05-28 02:36:33 +0430

    Add kakfa host and port to config file

--- manager/config.py
+++ manager/config.py
@@ -6,6 +6,8 @@ PYTHONPATH = os.getenv("PYTHONPATH")
 DATABASE_URL = os.getenv("DATABASE_URL")
 TOKEN_EXPIRE_TIME = int(os.getenv('TOKEN_EXPIRE_TIME', 60 * 60 * 24))
 MODULE_DOWN_TIMEOUT = int(os.getenv('MODULE_DOWN_TIMEOUT', 60 * 60))
+KAFKA_HOST = os.getenv('KAFKA_HOST')
+KAFKA_PORT = os.getenv('KAFKA_PORT')
 
 TORTOISE_ORM = {
     "connections": {"default": DATABASE_URL},
