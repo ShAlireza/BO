4abbe81df22f93b2988544c3d1bf4fa8d73e988c
alireza <alirezashateri7@gmail.com>
2021-05-29 20:06:52 +0430

    Update generate_full_command to contain kafka_host and kafka_port

--- scheduler/data/pydantic/models.py
+++ scheduler/data/pydantic/models.py
@@ -6,12 +6,24 @@ from uuid import UUID
 from pydantic import BaseModel, Field, validator, ValidationError
 
 from bo_shared.models.scheduler import (
-    CronJob,
+    CronJob as CronJobShared,
     CronJobPost,
     CronJobResponse,
     CronJobPatch
 )
 
-from config import PYTHONPATH
+from config import PYTHONPATH, KAFKA_HOST, KAFKA_PORT
 
 __all__ = ('CronJob', 'CronJobPost', 'CronJobResponse', 'CronJobPatch')
+
+
+class CronJob(CronJobShared):
+    def generate_full_command(self):
+        self.full_command = (f'python {PYTHONPATH}/internal/event_push.py '
+                             f'--id {self.id} '
+                             f'--tech {self.technology} '
+                             f'--host {self.host} '
+                             f'--port {self.port} '
+                             f'--kafka-host {KAFKA_HOST} '
+                             f'--kafka-port {KAFKA_PORT}')
+        return self.full_command
