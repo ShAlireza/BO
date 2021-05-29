97cdfca23a93c6a207b36ad8fb269ddc95096192
alireza <alirezashateri7@gmail.com>
2021-05-29 20:06:06 +0430

    Add kafka configs to config.py and .env file

--- scheduler/config.py
+++ scheduler/config.py
@@ -4,6 +4,8 @@ from tortoise import Tortoise
 SCHEDULER_USER = os.getenv("SCHEDULER_USER")
 PYTHONPATH = os.getenv("PYTHONPATH")
 DATABASE_URL = os.getenv("DATABASE_URL")
+KAFKA_HOST = os.getenv('KAFKA_HOST')
+KAFKA_PORT = os.getenv('KAFKA_PORT')
 
 TORTOISE_ORM = {
     "connections": {"default": DATABASE_URL},
