ffbbd4b928d230ff2e5151b041f02c3f7f087c1e
alireza <alirezashateri7@gmail.com>
2021-05-29 20:07:36 +0430

    Add producer to push new events on kafka

--- scheduler/internal/event_push.py
+++ scheduler/internal/event_push.py
@@ -1,9 +1,25 @@
+import json
 import argparse
+
+from confluent_kafka import Producer
+
+# TODO
+#  1. log push errors or success with logger
+#  2. ...
 
 parser = argparse.ArgumentParser(
     prog='scheduler event pusher',
     description='push an event to queue',
     allow_abbrev=False
+)
+
+parser.add_argument(
+    '-i',
+    '--id',
+    metavar='id',
+    type=str,
+    help='service cronjob id',
+    required=True
 )
 
 parser.add_argument(
@@ -33,16 +49,59 @@ parser.add_argument(
     required=True
 )
 
+parser.add_argument(
+    '--kafka-host',
+    metavar='kafka_host',
+    type=str,
+    help='kafka host',
+    required=True
+)
+
+parser.add_argument(
+    '--kafka-port',
+    metavar='kafka_port',
+    type=str,
+    help='kafka port',
+    required=True
+)
+
 if __name__ == '__main__':
     args = parser.parse_args()
 
     push_data = {
+        'id': args.id,
         'tech': args.tech,
         'host': args.host,
         'port': args.port
     }
 
     print(push_data)
+
+    conf = {'bootstrap.servers': f'{args.kafka_host}:{args.kafka_port}'}
+
+    producer = Producer(**conf)
+
+
+    def push_callback(error, message):
+        if error:
+            raise Exception(error)
+        else:
+            print(f'Messages delivered to {message.topic()}'
+                  f' [{message.partition()}]'
+                  f' @ {message.offset()}')
+
+
+    try:
+        producer.produce(
+            args.tech,
+            json.dumps(push_data).encode('utf-8'),
+            callback=push_callback
+        )
+    except BufferError as e:
+        raise e
+
+    producer.poll(0)
+    producer.flush()
 
 
 def get_parser():
