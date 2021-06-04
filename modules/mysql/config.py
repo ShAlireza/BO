9a52acc56d274ff93af84dbf693a1ab600c0fa99
alireza <alirezashateri7@gmail.com>
2021-06-05 02:48:25 +0430

    Add consumer to mysql module

--- modules/mysql/config.py
+++ modules/mysql/config.py
@@ -1,12 +1,13 @@
 import os
 
-SECRET_KEY = "MoyIgPkLQu-yxh@1VM1JMyWD#bV3BR!gXON2eD=dJHyh3vP86Szn3zrO-<6_-t6Z"
+SECRET_KEY = "-qBLt7PhmITXYXFQG+>U>r?*=+Pq<Ne$su=jQqWux7k!jRx$lP#3*Pr63+wyYi3O"
 MANAGER_HOST = 'localhost'
-MANAGER_PORT = '8000'
+MANAGER_PORT = '9000'
 LOGIN_PATH = '/api/module/login'
 HOST = os.getenv('HOST')
 PORT = os.getenv('PORT')
 NAME = 'mysql'
+VALID_CREDENTIAL_NAMES = 'username,password'
 KAFKA_HOST = None
 KAFKA_PORT = None
 KAFKA_TOPIC = None
--- modules/mysql/main.py
+++ modules/mysql/main.py
@@ -1,7 +1,14 @@
 from bo_shared.modules.app import create_module_app
 
+
 import config
 
 app = create_module_app()
 
 print(config.KAFKA_HOST, config.KAFKA_PORT, config.KAFKA_TOPIC)
+
+from module import MySqlModule
+
+module = MySqlModule()
+
+module()
