bf1909f0bf3616d87a03d6634bdb5882688b8b53
alireza <alirezashateri7@gmail.com>
2021-06-10 18:48:04 +0430

    Finished mysql backup process

--- modules/mysql/main.py
+++ modules/mysql/main.py
@@ -1,3 +1,5 @@
+import threading
+
 from bo_shared.modules.interface import Consumer
 from bo_shared.modules.app import create_module_app
 
@@ -7,12 +9,10 @@ import config
 
 app = create_module_app()
 
-print('rabbitmq_host, rabbitmq_port, rabbitmq_queue')
-print(config.RABBITMQ_HOST, config.RABBITMQ_PORT, config.RABBITMQ_QUEUE)
-
+print(config.RABBITMQ_HOST, config.RABBITMQ_PORT, config.RABBITMQ_QUEUE,
+      config.MINIO_ADDRESS, config.MINIO_ACCESS_KEY, config.MINIO_SECRET_KEY)
 
 consumer = Consumer(
     module_class=MySqlModule
 )
-
-consumer()
+consumer.start()
--- modules/mysql/module.py
+++ modules/mysql/module.py
@@ -1,25 +1,46 @@
+import subprocess
+import os
+from datetime import datetime
+
 from bo_shared.modules.interface import BaseModule
 from minio import Minio
 
-mysql_dump = f'/usr/bin/mysqldump -u root'
+from config import MINIO_CLIENT
 
+
+# Todo
+#  1. change os.system to subprocess.Popen
+#  2. ...
 
 class MySqlModule(BaseModule):
 
     def backup(self):
-        print(f'backing up with data {self.data}')
-        pass
+        print(f'backing up with data {self.flat_data}')
+        host = self.flat_data.get('host')
+        port = self.flat_data.get('port')
+        username = self.flat_data.get('username')
+        password = self.flat_data.get('password')
+        label = self.flat_data.get('label')
+        module = self.flat_data.get('module')
+
+        path = self.create_necessary_bucket_minio(
+            label=label,
+            module=module
+        )
+
+        filename = f'{path}/{label}-{module}-{datetime.now().isoformat()}'
+        return_code = os.system(
+            f'mysqldump -h {host} -P {port} --protocol=tcp -u {username} '
+            f'-p{password} --all-databases | {MINIO_CLIENT} pipe {filename}'
+        )
+
+        if return_code != 0:
+            print(f'backup failed with return code: {return_code}')
+        else:
+            print(f'backup {module} successful: {host}:{port}')
 
     def validate(self):
         pass
 
     def restore(self):
         pass
-
-# def main():
-#     client = Minio(
-#         "http://172.16.18.242:9000",
-#         access_key="minio",
-#         secret_key="minio123",
-#     )
-#     client.make_bucket()
