ad2c59fa0ee7eca45187ae4a7b2ddcd68b0df473
alireza <alirezashateri7@gmail.com>
2021-06-15 02:37:27 +0430

    Add routes of namespace module

--- auth/routers/auth.py
+++ auth/routers/auth.py
@@ -4,3 +4,42 @@ from fastapi import APIRouter, Body, Path, Query
 
 router = APIRouter()
 
+
+@router.get('/token/namespace')
+def get_namespace():
+    pass
+
+
+@router.get('/namespace')
+def get_namespaces():
+    pass
+
+
+@router.post('/namespace')
+def create_namespace():
+    pass
+
+
+@router.delete('/namespace/{namespace_name')
+def delete_namespace():
+    pass
+
+
+@router.patch('/namespace/{namespace_name}')
+def edit_namespace():
+    pass
+
+
+@router.post('/token')
+def create_token():
+    pass
+
+
+@router.patch('/token/{token_id}')
+def toggle_token_validity():
+    pass
+
+
+@router.delete('/token/{token_id}')
+def delete_token():
+    pass
